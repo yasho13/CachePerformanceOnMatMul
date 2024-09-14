7e9fdfb82e4d11e7c70d9384a5c965a664a58a96
Ronit26x <150147594+Ronit26x@users.noreply.github.com>
2024-09-13 23:06:18 -0400

    Update gather.cpp

--- src/gather.cpp
+++ src/gather.cpp
@@ -9,39 +9,37 @@ using namespace std;
 
 void parsec_roi_begin() 
 {
-
 }
 
 void parsec_roi_end() 
 {
-	
 }
 
 struct Result {
-	vector< vector<int> > A;
+    vector< vector<int> > A;
 };
 
 Result read(string filename) {
-	vector< vector<int> > A;
-	Result ab;
-	string line;
-	ifstream infile;
-	infile.open (filename.c_str());
+    vector< vector<int> > A;
+    Result ab;
+    string line;
+    ifstream infile;
+    infile.open (filename.c_str());
 
-	int i = 0;
-	while (getline(infile, line) && !line.empty()) {
-		istringstream iss(line);
-		A.resize(A.size() + 1);
-		int a, j = 0;
-		while (iss >> a) {
-			A[i].push_back(a);
-			j++;
-		}
-		i++;
-	}
-	infile.close();
-	ab.A = A;
-	return ab;
+    int i = 0;
+    while (getline(infile, line) && !line.empty()) {
+        istringstream iss(line);
+        A.resize(A.size() + 1);
+        int a, j = 0;
+        while (iss >> a) {
+            A[i].push_back(a);
+            j++;
+        }
+        i++;
+    }
+    infile.close();
+    ab.A = A;
+    return ab;
 }
 
 vector<int> gather(const vector<vector<int>>& A) {
@@ -64,34 +62,32 @@ vector<int> gather(const vector<vector<int>>& A) {
     return gathered_data;  // Return the gathered data
 }
 
-
-
-void printMatrix(vector< vector<int> > matrix) {
-	vector< vector<int> >::iterator it;
-	vector<int>::iterator inner;
-	for (it=matrix.begin(); it != matrix.end(); it++) {
-		for (inner = it->begin(); inner != it->end(); inner++) {
-			cout << *inner;
-			if(inner+1 != it->end()) {
-				cout << "\t";
-			}
-		}
-		cout << endl;
-	}
+// Updated to print a 1D vector
+void printVector(const vector<int>& vec) {
+    for (size_t i = 0; i < vec.size(); i++) {
+        cout << vec[i];
+        if (i + 1 != vec.size()) {
+            cout << "\t";
+        }
+    }
+    cout << endl;
 }
 
 int main (int argc, char* argv[]) {
-	srand(time(0));
-	string filename;
-	if (argc < 3) {
-		filename = "2000.in";
-	} else {
-		filename = argv[2];
-	}
-	Result result = read (filename);
+    srand(time(0));
+    string filename;
+    if (argc < 3) {
+        filename = "2000.in";
+    } else {
+        filename = argv[2];
+    }
+    Result result = read (filename);
     parsec_roi_begin();
-	vector< vector<int> > C = gather(result.A);
+    vector<int> C = gather(result.A);  // Corrected to vector<int>
     parsec_roi_end();
-	//printMatrix(C);
-	return 0;
+    
+    // Optionally print the gathered data as a 1D vector
+    //printVector(C);
+    
+    return 0;
 }
